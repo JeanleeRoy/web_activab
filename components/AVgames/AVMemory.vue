09ca42dea59aea41b1a77f63121333c3433d8a18
Jeanlee Barreto <jeanlee-roy@hotmail.com>
2023-03-23 18:01:35 -0500

    feat: add AVMemory game

---
+++ components/AVgames/AVMemory.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <MemoryGame
+      v-if="cards?.length"
+      :pairs="pairs"
+      :cards="cards"
+      :initial-hint-time="1800"
+      @completed="onCompleted"
+    />
+  </div>
+</template>
+
+<script>
+import MemoryGame from '~/components/memoryGame.vue'
+
+export default {
+  name: 'AVMemory',
+  emits: ['is-completed'],
+  components: { MemoryGame },
+  props: {
+    pairs: {
+      type: Number,
+      default: 4,
+    },
+    cards: {
+      type: Array,
+      default: () => [],
+    },
+    coverImage: {
+      type: String,
+      default: 'img/a.jpg',
+    },
+  },
+  methods: {
+    onCompleted(val) {
+      // console.log('Memory Game completed', val)
+      this.$emit('is-completed', val)
+    },
+  },
+}
+</script>
+
+<style scoped></style>
